f29fd4d1219ad98eb57ace6d72052d406ca13c60
c-stumph <clstumph@gmail.com>
2026-07-07 12:01:27 -0700

    Add files via upload

---
+++ Sensor Code/GY271toSDworkingcode/GY271toSDworkingcode.ino
@@ -0,0 +1,67 @@
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <QMC5883LCompass.h>
+
+QMC5883LCompass compass;
+File myFile;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  compass.init();
+  compass.setMode(0x01,0x00,0x00,0x00);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while(1);
+  }
+Serial.println("initialization done.");
+
+Serial.println("Magnetic field X (mG),Magnetic field Y (mG),Magnetic field Z (mG),Heading (°),Magnitude (mG)");
+
+myFile = SD.open("data.csv", FILE_WRITE);
+myFile.println("Magnetic field X (mG),Magnetic field Y (mG),Magnetic field Z (mG),Heading (°),Magnitude (mG)");
+myFile.close();
+}
+
+void loop() {
+
+  myFile = SD.open("data.csv", FILE_WRITE);
+
+  compass.read();
+  int x = compass.getX();
+  int y = compass.getY();
+  int z = compass.getZ();
+  int heading = compass.getAzimuth();
+
+float magnitude = sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+
+  Serial.print(x/120); 
+  Serial.print(",");
+  Serial.print(y/120); 
+  Serial.print(",");
+  Serial.print(z/120); 
+  Serial.print(",");
+  Serial.print(heading);
+  Serial.print(",");
+  Serial.print(magnitude/120); 
+  Serial.println(",");
+  Serial.println();
+
+  myFile.print(x/120); 
+  myFile.print(",");
+  myFile.print(y/120); 
+  myFile.print(",");
+  myFile.print(z/120); 
+  myFile.print(",");
+  myFile.print(heading);
+  myFile.print(",");
+  myFile.print(magnitude/120); 
+  myFile.println(",");
+
+  myFile.close();
+  delay(1000);
+}
