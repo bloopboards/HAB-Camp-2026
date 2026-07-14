6b957caea801df8efc8d5135ca253f0d3412354f
Samuel Le <electroven1121@gmail.com>
2026-07-14 15:21:39 -0700

    fixed csv formatting?

--- 2026 HAB Camp Code/Sensor Code/GY271_Magnetometer_toSD/GY271toSDworkingcode.ino
+++ 2026 HAB Camp Code/Sensor Code/GY271_Magnetometer_toSD/GY271toSDworkingcode.ino
@@ -20,10 +20,10 @@ void setup() {
   }
 Serial.println("initialization done.");
 
-Serial.println("Magnetic field X (mG),Magnetic field Y (mG),Magnetic field Z (mG),Heading (°),Magnitude (mG)");
+Serial.println("Magnetic field X (mG), Magnetic field Y (mG), Magnetic field Z (mG), Heading (°), Magnitude (mG)");
 
 myFile = SD.open("data.csv", FILE_WRITE);
-myFile.println("Magnetic field X (mG),Magnetic field Y (mG),Magnetic field Z (mG),Heading (°),Magnitude (mG)");
+myFile.println("Magnetic field X (mG), Magnetic field Y (mG), Magnetic field Z (mG), Heading (°), Magnitude (mG)");
 myFile.close();
 }
 
